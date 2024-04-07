ae9dd4ae2676a390e96d184626f54cf9660dc736
Lautaro Farias <farias.lautaro002@gmail.com>
2024-04-07 20:00:25 -0300

    style: Apply prettier on app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,25 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from "next/font/google"
 
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from "next"
+
+import "./globals.css"
+
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "cURLtter",
   description: "Yet another URL s̶h̶o̶r̶t̶e̶n̶e̶r̶ cutter.",
-  icons: ["/favicon.svg"]
-  
-};
+  icons: ["/favicon.svg"],
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body className={`${inter.className} dark`}>{children}</body>
     </html>
-  );
+  )
 }
