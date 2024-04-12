57e63d783951e013dc4c9b16628d0210eb7226f8
Lautaro Farias <farias.lautaro002@gmail.com>
2024-04-12 20:50:04 -0300

    refactor: Create Loader component

--- src/components/URLForm/URLFormContainer.tsx
+++ src/components/URLForm/URLFormContainer.tsx
@@ -1,6 +1,6 @@
+import Loader from "@/components/Loader"
 import { URLForm } from "@/components/URLForm/URLForm"
 import { FormStatus, useSubmit } from "@/components/URLForm/useSubmit"
-import styles from "@/styles/loader.module.css"
 
 export default function URLFormContainer() {
   const { formStatus, onSubmit } = useSubmit()
@@ -8,9 +8,7 @@ export default function URLFormContainer() {
   return (
     <>
       {formStatus == FormStatus.base && <URLForm onSubmit={onSubmit} />}
-      {formStatus == FormStatus.loading && (
-        <div className={styles.loader}>https://...</div>
-      )}
+      {formStatus == FormStatus.loading && <Loader />}
     </>
   )
 }
